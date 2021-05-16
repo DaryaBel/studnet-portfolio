e0400266b3186d95355f2d8173daf34e7b19540e
Darya Belyaeva <d.belyaeva1@gmail.com>
2021-05-16 21:52:42 +0400

    smth

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Дарья\Desktop\УНИВЕР\5 семестр\ИП\ip\venv\Scripts\python.exe
+#!e:\универ\3 курс\5 семестр\ип\portfolio\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
