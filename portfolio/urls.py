825b31b16fbfee2d7930b55a9cbafb8aab9844d6
Darya Belyaeva <d.belyaeva1@gmail.com>
2021-06-15 14:08:07 +0300

    DEV: start of frontend development

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('rest-api/', include('rest_framework.urls')),
     path('api/v1/', include('app.urls')),
     path('api/v1/', include('events.urls')),
